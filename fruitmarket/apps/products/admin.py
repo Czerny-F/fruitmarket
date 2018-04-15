6298c95c457297b537130c77647abfdaa35d562b
Czerny-F <dummy00.cz@gmail.com>
2018-04-16 02:25:11 +0900

    products: fruit model and admin

--- fruitmarket/apps/products/admin.py
+++ fruitmarket/apps/products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Fruit
 
-# Register your models here.
+
+@admin.register(Fruit)
+class FruitAdmin(admin.ModelAdmin):
+    readonly_fields = ('updated_at', 'created_at')
+    list_display = ('name', 'unit_price', 'updated_at', 'created_at')
--- fruitmarket/apps/products/models.py
+++ fruitmarket/apps/products/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from fruitmarket.core.models import EditableModel
 
-# Create your models here.
+
+class Fruit(EditableModel):
+    name = models.CharField(_("name"), max_length=256, unique=True)
+    unit_price = models.PositiveIntegerField(_("unit price"))
+
+    class Meta:
+        verbose_name = _("fruit")
+        verbose_name_plural = _("fruits")
+        ordering = ['-updated_at', '-created_at']
+        indexes = [
+            models.Index(fields=['updated_at', 'created_at'],
+                         name='products_fruit_timestamp_idx'),
+        ]
+
+    def __str__(self):
+        return self.name
