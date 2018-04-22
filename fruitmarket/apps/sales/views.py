823d26d2c680903ec896b1ca51ec3937df6143a1
Czerny-F <dummy00.cz@gmail.com>
2018-04-23 03:06:58 +0900

    sales: stats view

--- fruitmarket/apps/sales/views.py
+++ fruitmarket/apps/sales/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import FruitSales
 
-# Create your views here.
+
+class FruitSalesStats(generic.TemplateView):
+    template_name = 'sales/stats.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['total'] = FruitSales.total()
+        return super().get_context_data(**kwargs)
