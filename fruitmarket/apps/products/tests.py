81af1f09d374e62659b12004709c948d824731c6
Czerny-F <dummy00.cz@gmail.com>
2018-04-23 11:08:26 +0900

    products: tests

--- fruitmarket/apps/products/tests.py
+++ fruitmarket/apps/products/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from .models import Fruit
 
-# Create your tests here.
+
+def create_fruits(cls):
+    cls.blueberry = Fruit.objects.create(name='blueberry', unit_price=300)
+    cls.lemon = Fruit.objects.create(name='lemon', unit_price=80)
+    cls.apple = Fruit.objects.create(name='apple', unit_price=90)
+
+
+class FruitTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        create_fruits(cls)
+
+    def setUp(self):
+        pass
+
+    def test_str(self):
+        self.assertEqual(str(self.blueberry), 'blueberry')
+        self.assertEqual(str(self.lemon), 'lemon')
+        self.assertEqual(str(self.apple), 'apple')
