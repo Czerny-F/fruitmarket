7b6a9a442a7b3f990ea1578d23342dc934b0c055
Czerny-F <dummy00.cz@gmail.com>
2018-05-05 04:06:55 +0900

    temporary index view

--- fruitmarket/urls.py
+++ fruitmarket/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from .core import views
 
 urlpatterns = [
+    path('', views.Index.as_view(), name='index'),
     path('products/', include('fruitmarket.apps.products.urls')),
     path('sales/', include('fruitmarket.apps.sales.urls')),
     path('admin/', admin.site.urls),
