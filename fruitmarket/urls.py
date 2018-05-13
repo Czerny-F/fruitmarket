ae65d84df8cbe60255fc0b54e62cf0707724f9ff
Czerny-F <dummy00.cz@gmail.com>
2018-05-14 00:13:48 +0900

    login/out views

--- fruitmarket/urls.py
+++ fruitmarket/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from .core import views
 
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
+    path('login/', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('products/', include('fruitmarket.apps.products.urls')),
     path('sales/', include('fruitmarket.apps.sales.urls')),
     path('admin/', admin.site.urls),
