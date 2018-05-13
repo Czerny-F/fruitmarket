7f7f7d39e12bf61d14347ef63ddb1a8301ffb351
Czerny-F <dummy00.cz@gmail.com>
2018-05-14 00:22:06 +0900

    core: index view without inheritance

--- fruitmarket/urls.py
+++ fruitmarket/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
 from django.urls import path, include
-from .core import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', views.Index.as_view(), name='index'),
+    path('', login_required(TemplateView.as_view(template_name='core/index.html')), name='index'),
     path('login/', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('products/', include('fruitmarket.apps.products.urls')),
