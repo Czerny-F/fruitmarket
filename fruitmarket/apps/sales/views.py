fb2a4bee83d38330678342813a61b8bc60fde625
Czerny-F <dummy00.cz@gmail.com>
2018-05-23 18:27:26 +0900

    sales: refactor fruitsales stats view

--- fruitmarket/apps/sales/views.py
+++ fruitmarket/apps/sales/views.py
@@ -1,3 +1,4 @@
+import datetime
 from django.views import generic
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
@@ -51,56 +52,37 @@ class FruitSalesDelete(FruitSalesEditMixin, generic.DeleteView):
 
 
 @method_decorator(login_required, name='dispatch')
-class FruitSalesStatsOverview(generic.TodayArchiveView):
-    queryset = FruitSales.objects.all()
+class FruitSalesStatsOverview(generic.dates.MonthMixin,
+                              generic.dates.DayMixin,
+                              generic.dates.DateMixin,
+                              generic.ListView):
+    model = FruitSales
+    stats_class = FruitSalesStats
     date_field = 'sold_at'
     allow_empty = True
-    template_name = 'sales/stats.html'
+    template_name = 'sales/fruitsales_stats.html'
 
     def get_context_data(self, **kwargs):
-        date = kwargs['day']
+        date = datetime.date.today()
         kwargs.update({
-            'gross': FruitSales.objects.gross(),
+            'gross': self.model.objects.gross(),
             'monthly': self.get_monthly_stats(date),
             'daily': self.get_daily_stats(date),
         })
         return super().get_context_data(**kwargs)
 
-    def get_monthly_stats(self, date):
-        for lookup in self._make_monthly_lookups(date):
-            yield {
-                'date': lookup['date'],
-                'stats': FruitSalesStats(self.get_dated_queryset(**lookup['kwargs'])),
-            }
-
-    def _make_monthly_lookups(self, date, months=3):
+    def get_monthly_stats(self, date, months=3):
         for _ in range(months):
             yield {
                 'date': self._get_current_month(date),
-                'kwargs': self._make_single_month_lookup(date),
+                'stats': self.stats_class(self.model.objects.monthly(date)),
             }
             date = self.get_previous_month(date)
 
-    def _make_single_month_lookup(self, date) -> dict:
-        date_field = self.get_date_field()
-        since = self._make_date_lookup_arg(self._get_current_month(date))
-        until = self._make_date_lookup_arg(self._get_next_month(date))
-        return {
-            '%s__gte' % date_field: since,
-            '%s__lt' % date_field: until,
-        }
-
-    def get_daily_stats(self, date):
-        for lookup in self._make_daily_lookups(date):
-            yield {
-                'date': lookup['date'],
-                'stats': FruitSalesStats(self.get_dated_queryset(**lookup['kwargs'])),
-            }
-
-    def _make_daily_lookups(self, date, days=3):
+    def get_daily_stats(self, date, days=3):
         for _ in range(days):
             yield {
                 'date': date,
-                'kwargs': self._make_single_date_lookup(date),
+                'stats': self.stats_class(self.model.objects.daily(date)),
             }
             date = self.get_previous_day(date)
